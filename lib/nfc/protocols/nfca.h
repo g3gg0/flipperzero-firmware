e5b3a12e1016859c47523fcd34b3e5d3d69d2bf9
g3gg0 <git@g3gg0.de>
2023-06-03 15:48:53 +0200

    switch NfcA to use DigitalSequence as well

--- lib/nfc/protocols/nfca.h
+++ lib/nfc/protocols/nfca.h
@@ -6,9 +6,10 @@
 #include <lib/digital_signal/digital_signal.h>
 
 typedef struct {
-    DigitalSignal* one;
-    DigitalSignal* zero;
-    DigitalSignal* tx_signal;
+    DigitalSignal* seq_d; /* sequence D, modulation with subcarrier during first half */
+    DigitalSignal* seq_e; /* sequence E, modulation with subcarrier during second half */
+    DigitalSignal* seq_f; /* sequence F, no modulation at all */
+    DigitalSequence* tx_signal;
 } NfcaSignal;
 
 uint16_t nfca_get_crc16(uint8_t* buff, uint16_t len);
